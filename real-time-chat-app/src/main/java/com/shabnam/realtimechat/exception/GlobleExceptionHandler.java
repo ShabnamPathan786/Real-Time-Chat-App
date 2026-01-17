81c1ceba0dd125da4ddfb1a383fe393a3e10f7ce
ShabnamPathan786 <sp83348899@gmail.com>
2026-01-17 17:52:26 +0530

    feat: add custom exceptions for error handling
    - RoomNotFoundException: thrown when room doesn't exist
    - RoomAlreadyExistsException: thrown for duplicate rooms

--- real-time-chat-app/src/main/java/com/shabnam/realtimechat/exception/GlobleExceptionHandler.java
+++ real-time-chat-app/src/main/java/com/shabnam/realtimechat/exception/GlobleExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.shabnam.realtimechat.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobleExceptionHandler {
+
+    @ExceptionHandler(RoomNotFoundException.class)
+    public ResponseEntity<String> handleRoomNotFound(RoomNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RoomAlreadyExistsException.class)
+    public ResponseEntity<String> handleRoomAlreadyExists(RoomAlreadyExistsException ex){
+       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
 }
